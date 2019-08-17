30961218ab6075b960d2c3657dfd15482a4395d7
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-08-17 18:38:51 +0530

    add assignment 3, generating tinyC lexer

---
+++ Assignment3_Lexer/ass3_17CS30033.y
@@ -0,0 +1,25 @@
+%{ /* C Declarations and Definitions */
+#include <stdio.h>
+
+extern int yylex();
+void yyerror(char *s);
+
+%}
+
+%union {
+int intval;
+}
+
+%token <intval> KEYWORD
+%token <intval> IDENTIFIER
+%token <intval> CONSTANT
+%token <intval> STRING_LITERAL
+%token <intval> PUNCTUATOR
+
+%%
+statement   :|KEYWORD|IDENTIFIER|CONSTANT|STRING_LITERAL|PUNCTUATOR
+%%
+
+void yyerror(char *s) {
+	printf ("ERROR is %s", s);
+}
---
+++ Assignment3_Lexer/ass3_17CS30033_test.c
@@ -0,0 +1,93 @@
+
+#include<stdio.h>
+#include<math.h>
+
+//Shivam Kumar Jha
+
+/* 17CS30033
+Compilers assignment 3
+ ****/
+
+const
+continue
+default
+do
+double
+auto
+break
+case
+char
+register
+restrict
+return
+short
+signed
+sizeof
+static
+struct
+volatile
+while
+else
+enum
+extern
+switch
+typedef
+union
+unsigned
+void
+float
+for
+goto
+if
+inline
+int
+long
+
+int integer = 10000;
+float flt = 51.6;
+flt = .6;
+flt = 12e13;
+
+char var = 'k' ;
+
+int a = 5;
+int b = 5;
+
+if ( a == b ){
+	printf("a == b");
+}
+
+
+for(i=0;i<=10;i++){
+	printf("%d", i);
+}
+i=j*k;
+
+[
+]
+++
+/
+?
+=
+,
+]
+(
+{ } . ->
+* + - ~ !
+% << >> < > <= >=
+: ;
+*= /= %= += -= <<=
+#
+--
+)
+&
+==
+>>=
+!=
+&=
+^
+|
+^=
+&&
+||
+|=
